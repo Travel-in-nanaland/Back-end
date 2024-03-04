7effeed8c35f66fc854ed76427b97a7b396cf3cb
seokhee <skdlzl753@naver.com>
2024-03-04 21:11:32 +0900

    init project

---
+++ src/main/java/com/jeju/nanaland/NanalandApplication.java
@@ -0,0 +1,13 @@
+package com.jeju.nanaland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanalandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanalandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeju/nanaland/NanalandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeju.nanaland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanalandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
