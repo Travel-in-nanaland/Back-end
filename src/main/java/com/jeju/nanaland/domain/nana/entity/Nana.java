958d3e084d4b43ce3eead8b95283b6553bbaf091
seokhee <skdlzl753@naver.com>
2024-03-22 14:01:20 +0900

    [#3] feat: Nana, NanaTrans 엔티티 추가

--- src/main/java/com/jeju/nanaland/domain/nana/entity/Nana.java
+++ src/main/java/com/jeju/nanaland/domain/nana/entity/Nana.java
@@ -1,6 +1,7 @@
 package com.jeju.nanaland.domain.nana.entity;
 
 import com.jeju.nanaland.domain.common.entity.BaseEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import java.util.List;
@@ -14,6 +15,6 @@ public class Nana extends BaseEntity {
 
   String imageUrl;
 
-  @OneToMany(mappedBy = "nana")
+  @OneToMany(mappedBy = "nana", cascade = CascadeType.REMOVE, orphanRemoval = true)
   private List<NanaTrans> nanaTrans;
 }
