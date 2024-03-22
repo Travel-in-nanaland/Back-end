d27dd173f7cbabd7aa349866521ac6c02f4c72b7
jyajoo <jyajoo1020@gmail.com>
2024-03-22 09:23:56 +0900

    feat: BaseEntity 추가 (#1)

--- src/main/java/com/jeju/nanaland/NanalandApplication.java
+++ src/main/java/com/jeju/nanaland/NanalandApplication.java
@@ -2,8 +2,10 @@ package com.jeju.nanaland;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class NanalandApplication {
 
 	public static void main(String[] args) {
