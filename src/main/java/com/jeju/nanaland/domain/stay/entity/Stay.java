7da8f2fb123c6ddd309b53794282423ec286c4bd
seokhee <skdlzl753@naver.com>
2024-03-22 13:55:21 +0900

    [#3] feat: stay, stay_trans 엔티티 추가

---
+++ src/main/java/com/jeju/nanaland/domain/stay/entity/Stay.java
@@ -0,0 +1,27 @@
+package com.jeju.nanaland.domain.stay.entity;
+
+import com.jeju.nanaland.domain.common.entity.BaseEntity;
+import com.jeju.nanaland.domain.common.entity.Common;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Stay extends Common {
+
+  private Integer price;
+
+  private String homepage;
+
+  private String parking;
+
+  private Integer rating;
+
+  @OneToMany(mappedBy = "stay", cascade = CascadeType.REMOVE, orphanRemoval = true)
+  private List<StayTrans> stayTrans;
+}
