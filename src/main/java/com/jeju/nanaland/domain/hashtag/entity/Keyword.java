c2aba913a0372de74e268940696d9b7048d44c2b
jyajoo <jyajoo1020@gmail.com>
2024-03-25 17:45:11 +0900

    [#6] feat: Keyword에 @Builder 추가

--- src/main/java/com/jeju/nanaland/domain/hashtag/entity/Keyword.java
+++ src/main/java/com/jeju/nanaland/domain/hashtag/entity/Keyword.java
@@ -4,11 +4,15 @@ import com.jeju.nanaland.domain.common.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Keyword extends BaseEntity {
 
