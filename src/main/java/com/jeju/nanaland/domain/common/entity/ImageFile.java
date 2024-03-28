8320d168390bdc2a9f79c90f7da05f12debe5a80
seokhee <skdlzl753@naver.com>
2024-03-28 16:30:04 +0900

    [#14] feat: ImageFile 엔티티 추가

--- src/main/java/com/jeju/nanaland/domain/common/entity/ImageFile.java
+++ src/main/java/com/jeju/nanaland/domain/common/entity/ImageFile.java
@@ -1,2 +1,24 @@
-package com.jeju.nanaland.domain.common.entity;public class ImageFile {
+package com.jeju.nanaland.domain.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class ImageFile {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String thumbnailUrl;
+
+  @Column(nullable = false)
+  private String originUrl;
 }
