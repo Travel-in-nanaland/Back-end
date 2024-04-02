97f2b9b3883f3bbd0891384f7679c5300105be1f
teho <xogh8755@naver.com>
2024-04-02 14:25:21 +0900

    [#5] refactor: exception 에러 메세지 ErrorCode의 메세지 사용

--- src/main/java/com/jeju/nanaland/global/exception/UnauthorizedException.java
+++ src/main/java/com/jeju/nanaland/global/exception/UnauthorizedException.java
@@ -1,9 +1,13 @@
 package com.jeju.nanaland.global.exception;
 
-public class UnauthorizedException extends RuntimeException{
+public class UnauthorizedException extends RuntimeException {
 
-//로그인 유저가 아닐 경우, admin 개발되면 권한 구분
+  //로그인 유저가 아닐 경우, admin 개발되면 권한 구분
   public UnauthorizedException() {
-    super("사용 권한이 없습니다.");
+    super(ErrorCode.UNAUTHORIZED_USER.getMessage());
+  }
+
+  public UnauthorizedException(String errorMessage) {
+    super(errorMessage);
   }
 }
