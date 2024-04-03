b0e1b4bb45e2ed1562f450aa6a74a3d9e48d74e9
jyajoo <jyajoo1020@gmail.com>
2024-04-03 13:09:14 +0900

    [#12] feat: SecurityConfig 추가

---
+++ src/main/java/com/jeju/nanaland/global/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.jeju.nanaland.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+    http
+        .csrf(AbstractHttpConfigurer::disable)
+        .httpBasic(AbstractHttpConfigurer::disable)
+        .formLogin(AbstractHttpConfigurer::disable)
+        .logout(AbstractHttpConfigurer::disable)
+        .cors(AbstractHttpConfigurer::disable)
+        .headers(c -> c.frameOptions(
+            FrameOptionsConfig::disable).disable())
+        .sessionManagement(c ->
+            c.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+    return http.build();
+  }
+}
