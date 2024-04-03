570855b0efff12a0f4674ff686a307f110249708
jyajoo <jyajoo1020@gmail.com>
2024-04-03 22:07:22 +0900

    [#12] feat: JwtProvider 추가
    accessToken, refreshToken 발급을 담당한다

---
+++ src/main/java/com/jeju/nanaland/global/jwt/JwtProvider.java
@@ -0,0 +1,56 @@
+package com.jeju.nanaland.global.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtProvider {
+
+  private static final String ACCESS_TOKEN_SUBJECT = "AccessToken";
+  private static final String REFRESH_TOKEN_SUBJECT = "RefreshToken";
+  private final SecretKey secretKey;
+  private final SecretKey secretKey2;
+
+  @Value("${jwt.access.expiration}")
+  private Long accessExpirationPeriod;
+  @Value("${jwt.refresh.expiration}")
+  private Long refreshExpirationPeriod;
+
+
+  public String getAccessToken(String email) {
+    Claims claims = Jwts.claims().setSubject(email);
+
+    Date now = new Date();
+    Date expiration = new Date(now.getTime() + accessExpirationPeriod);
+
+    return Jwts.builder()
+        .setSubject(ACCESS_TOKEN_SUBJECT)
+        .setClaims(claims)
+        .setExpiration(expiration)
+        .signWith(secretKey, SignatureAlgorithm.HS512)
+        .compact();
+  }
+
+  public String getRefreshToken(String email) {
+    Claims claims = Jwts.claims().setSubject(email);
+
+    Date now = new Date();
+    Date expiration = new Date(now.getTime() + refreshExpirationPeriod);
+
+    return Jwts.builder()
+        .setSubject(REFRESH_TOKEN_SUBJECT)
+        .setClaims(claims)
+        .setExpiration(expiration)
+        .signWith(secretKey2, SignatureAlgorithm.HS512)
+        .compact();
+  }
+}
