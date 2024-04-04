6ef58629272dc062767cc455741cb8b32d2517b3
jyajoo <jyajoo1020@gmail.com>
2024-04-04 17:52:01 +0900

    [#12] feat: JWT 필터 추가, 토큰 검증 구현

--- src/main/java/com/jeju/nanaland/global/config/SecurityConfig.java
+++ src/main/java/com/jeju/nanaland/global/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.jeju.nanaland.global.config;
 
+import com.jeju.nanaland.global.jwt.JWTAuthenticationFilter;
+import com.jeju.nanaland.global.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -11,14 +14,18 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+  private final JwtProvider jwtProvider;
+
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+  public SecurityFilterChain securityFilterChain(HttpSecurity http,
+      AuthenticationConfiguration authenticationConfiguration) throws Exception {
 
     http
         .csrf(AbstractHttpConfigurer::disable)
@@ -30,11 +37,19 @@ public class SecurityConfig {
             FrameOptionsConfig::disable).disable())
         .sessionManagement(c ->
             c.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
     http
         .authorizeHttpRequests(authHttpRequests -> authHttpRequests
             .requestMatchers("/login")
             .permitAll()
             .anyRequest().authenticated());
+
+    http
+        .addFilterBefore(
+            new JWTAuthenticationFilter(authenticationConfiguration.getAuthenticationManager(),
+                jwtProvider),
+            UsernamePasswordAuthenticationFilter.class);
+
     return http.build();
   }
 
--- src/main/java/com/jeju/nanaland/global/jwt/JwtProvider.java
+++ src/main/java/com/jeju/nanaland/global/jwt/JwtProvider.java
@@ -1,6 +1,7 @@
 package com.jeju.nanaland.global.jwt;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
@@ -8,6 +9,9 @@ import javax.crypto.SecretKey;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -19,12 +23,15 @@ public class JwtProvider {
   private static final String REFRESH_TOKEN_SUBJECT = "RefreshToken";
   private final SecretKey secretKey;
   private final SecretKey secretKey2;
-
+  private final MemberDetailsService memberDetailsService;
   @Value("${jwt.access.expiration}")
   private Long accessExpirationPeriod;
   @Value("${jwt.refresh.expiration}")
   private Long refreshExpirationPeriod;
 
+  private SecretKey getSecretKey() {
+    return secretKey;
+  }
 
   public String getAccessToken(Long memberId) {
     Claims claims = Jwts.claims().setSubject(String.valueOf(memberId));
@@ -52,5 +59,32 @@ public class JwtProvider {
         .setExpiration(expiration)
         .signWith(secretKey2, SignatureAlgorithm.HS512)
         .compact();
+  }
+
+  public boolean verifyAccessToken(String accessToken) {
+    try {
+      Jws<Claims> claimsJws = Jwts.parserBuilder()
+          .setSigningKey(getSecretKey())
+          .build()
+          .parseClaimsJws(accessToken);
+
+      return !claimsJws.getBody().getExpiration().before(new Date());
+    } catch (Exception e) {
+      return false;
+    }
+  }
+
+  public String getMemberId(String token) {
+    return Jwts.parserBuilder()
+        .setSigningKey(getSecretKey())
+        .build()
+        .parseClaimsJws(token)
+        .getBody()
+        .getSubject();
+  }
+
+  public Authentication getAuthentication(String token) {
+    UserDetails userDetails = memberDetailsService.loadUserByUsername(getMemberId(token));
+    return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
   }
 }
