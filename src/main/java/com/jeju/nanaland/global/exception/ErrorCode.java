bb0e1f2321f8ee07001aff946383541397ef76f4
jyajoo <jyajoo1020@gmail.com>
2024-04-04 17:48:58 +0900

    [#12] feat: ErrorCode에 ACCESS_DENIED 추가

--- src/main/java/com/jeju/nanaland/global/exception/ErrorCode.java
+++ src/main/java/com/jeju/nanaland/global/exception/ErrorCode.java
@@ -16,7 +16,8 @@ public enum ErrorCode {
   REQUEST_VALIDATION_EXCEPTION(BAD_REQUEST, "입력 형태가 잘못된 요청입니다."),
   UNAUTHORIZED_USER(UNAUTHORIZED, "access token이 존재하지 않습니다."),
   EXPIRED_TOKEN(UNAUTHORIZED, "만료된 토큰입니다."),
-  UNSUPPORTED_FILE_FORMAT(UNSUPPORTED_MEDIA_TYPE, "적절하지 않은 확장자의 파일입니다.");
+  UNSUPPORTED_FILE_FORMAT(UNSUPPORTED_MEDIA_TYPE, "적절하지 않은 확장자의 파일입니다."),
+  ACCESS_DENIED(UNAUTHORIZED, "접근 권한이 없습니다.");
 
   private final HttpStatus httpStatus;
   private final String message;
