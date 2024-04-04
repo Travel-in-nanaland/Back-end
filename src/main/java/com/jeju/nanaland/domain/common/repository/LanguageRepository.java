50b5c93d135f82ac82a90c11506a9c6169117f0d
jyajoo <jyajoo1020@gmail.com>
2024-04-04 14:45:29 +0900

    [#12] feat: Member와 Language Repository 추가

---
+++ src/main/java/com/jeju/nanaland/domain/common/repository/LanguageRepository.java
@@ -0,0 +1,9 @@
+package com.jeju.nanaland.domain.common.repository;
+
+import com.jeju.nanaland.domain.common.entity.Language;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LanguageRepository extends JpaRepository<Language, Long> {
+
+}
+
