498a7fda3249da1aff02c657a66b28d777a09fc6
jyajoo <jyajoo1020@gmail.com>
2024-04-09 20:14:38 +0900

    [#26] feat: swaggerConfig jwt 설정 추가

--- src/main/java/com/jeju/nanaland/global/config/SwaggerConfig.java
+++ src/main/java/com/jeju/nanaland/global/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.jeju.nanaland.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,14 +17,22 @@ public class SwaggerConfig {
 
   @Bean
   public OpenAPI openAPI() {
-    
+
+    SecurityScheme securityScheme = new SecurityScheme()
+        .type(SecurityScheme.Type.HTTP).scheme("Bearer").bearerFormat("JWT")
+        .in(SecurityScheme.In.HEADER).name("Authorization");
+    SecurityRequirement securityRequirement = new SecurityRequirement().addList("JWT");
+
+    Components components = new Components().addSecuritySchemes("JWT", securityScheme);
+
     Info info = new Info()
         .title("NanaLand API 명세서")
         .description("nanaland in JEJU API 명세서입니다.")
         .version(version);
 
     return new OpenAPI()
-        .components(new Components())
-        .info(info);
+        .info(info)
+        .components(components)
+        .addSecurityItem(securityRequirement);
   }
 }
