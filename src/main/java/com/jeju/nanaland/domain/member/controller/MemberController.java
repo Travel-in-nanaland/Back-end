a9c1e9276c6eea647e4c650b57af0c9d02ce1891
jyajoo <jyajoo1020@gmail.com>
2024-04-09 20:41:06 +0900

    [#26] docs: MemberController에 swagger 추가

--- src/main/java/com/jeju/nanaland/domain/member/controller/MemberController.java
+++ src/main/java/com/jeju/nanaland/domain/member/controller/MemberController.java
@@ -5,6 +5,8 @@ import com.jeju.nanaland.domain.member.service.MemberLoginService;
 import com.jeju.nanaland.global.ApiResponse;
 import com.jeju.nanaland.global.exception.SuccessCode;
 import com.jeju.nanaland.global.jwt.dto.JwtResponseDto.JwtDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,16 +22,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/member")
+@Tag(name = "회원(Member) 컨트롤러", description = "회원(Member) API입니다.")
 public class MemberController {
 
   private final MemberLoginService memberLoginService;
 
+  @Operation(summary = "로그인", description = "로그인을 하면 JWT가 발급됩니다.")
   @PostMapping("/login")
   public ApiResponse<JwtDto> login(@RequestBody @Valid LoginDto loginDto) {
     JwtDto jwtDto = memberLoginService.login(loginDto);
     return ApiResponse.success(SuccessCode.LOGIN_SUCCESS, jwtDto);
   }
 
+  @Operation(summary = "AccessToken 재발급", description = "RefreshToken으로 AccessToken이 재발급됩니다.")
   @GetMapping("/reissue")
   public ApiResponse<String> reissue(
       @RequestHeader(HttpHeaders.AUTHORIZATION) String refreshToken) {
