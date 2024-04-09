a88edc5f52b3628babf5ff1ce02f2d4a99a3b2d1
jyajoo <jyajoo1020@gmail.com>
2024-04-10 01:51:48 +0900

    [#26] docs: memberController에 swagger 적용

--- src/main/java/com/jeju/nanaland/domain/member/controller/MemberController.java
+++ src/main/java/com/jeju/nanaland/domain/member/controller/MemberController.java
@@ -6,6 +6,9 @@ import com.jeju.nanaland.global.BaseResponse;
 import com.jeju.nanaland.global.exception.SuccessCode;
 import com.jeju.nanaland.global.jwt.dto.JwtResponseDto.JwtDto;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -22,12 +25,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/member")
-@Tag(name = "회원(Member) 컨트롤러", description = "회원(Member) API입니다.")
+@Tag(name = "회원(Member)", description = "회원(Member) API입니다.")
 public class MemberController {
 
   private final MemberLoginService memberLoginService;
 
   @Operation(summary = "로그인", description = "로그인을 하면 JWT가 발급됩니다.")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "성공"),
+      @ApiResponse(responseCode = "400", description = "필요한 입력이 없는 경우", content = @Content)
+  })
   @PostMapping("/login")
   public BaseResponse<JwtDto> login(@RequestBody @Valid LoginDto loginDto) {
     JwtDto jwtDto = memberLoginService.login(loginDto);
@@ -35,6 +42,11 @@ public class MemberController {
   }
 
   @Operation(summary = "AccessToken 재발급", description = "RefreshToken으로 AccessToken이 재발급됩니다.")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "성공"),
+      @ApiResponse(responseCode = "400", description = "존재하지 않는 회원인 경우", content = @Content),
+      @ApiResponse(responseCode = "401", description = "RefreshToken이 유효하지 않은 경우", content = @Content)
+  })
   @GetMapping("/reissue")
   public BaseResponse<String> reissue(
       @RequestHeader(HttpHeaders.AUTHORIZATION) String refreshToken) {
