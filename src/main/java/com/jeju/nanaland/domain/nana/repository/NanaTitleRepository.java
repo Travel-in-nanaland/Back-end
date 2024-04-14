c39635c9c37a212fdff676979394831c45c87e8f
teho <xogh8755@naver.com>
2024-04-14 16:22:58 +0900

    [#22] feat: 나나 Pick detail을 위한 repository 생성

---
+++ src/main/java/com/jeju/nanaland/domain/nana/repository/NanaTitleRepository.java
@@ -0,0 +1,12 @@
+package com.jeju.nanaland.domain.nana.repository;
+
+import com.jeju.nanaland.domain.common.entity.Language;
+import com.jeju.nanaland.domain.nana.entity.Nana;
+import com.jeju.nanaland.domain.nana.entity.NanaTitle;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NanaTitleRepository extends JpaRepository<NanaTitle, Long> {
+
+  Optional<NanaTitle> findNanaTitleByNanaAndLanguage(Nana nana, Language language);
+}
