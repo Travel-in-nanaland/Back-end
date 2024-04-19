18d4980909d474e016f1dcffadfbdd475101c58a
seokhee <skdlzl753@naver.com>
2024-04-19 12:42:39 +0900

    [#54] feat: experience like toggle test

--- src/test/java/com/jeju/nanaland/domain/experience/service/ExperienceServiceTest.java
+++ src/test/java/com/jeju/nanaland/domain/experience/service/ExperienceServiceTest.java
@@ -1,5 +1,115 @@
 package com.jeju.nanaland.domain.experience.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.jeju.nanaland.domain.common.entity.Category;
+import com.jeju.nanaland.domain.common.entity.ImageFile;
+import com.jeju.nanaland.domain.common.entity.Language;
+import com.jeju.nanaland.domain.common.entity.Locale;
+import com.jeju.nanaland.domain.experience.entity.Experience;
+import com.jeju.nanaland.domain.favorite.entity.Favorite;
+import com.jeju.nanaland.domain.favorite.repository.FavoriteRepository;
+import com.jeju.nanaland.domain.member.entity.Member;
+import com.jeju.nanaland.domain.member.entity.Provider;
+import jakarta.persistence.EntityManager;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
 class ExperienceServiceTest {
 
+  @Autowired
+  EntityManager em;
+  @Autowired
+  ExperienceService experienceService;
+  @Autowired
+  FavoriteRepository favoriteRepository;
+
+  Language language;
+  Member member1, member2;
+  Experience experience;
+  Category category;
+
+  @BeforeEach
+  void init() {
+    ImageFile imageFile1 = ImageFile.builder()
+        .originUrl("origin")
+        .thumbnailUrl("thumbnail")
+        .build();
+    em.persist(imageFile1);
+
+    ImageFile imageFile2 = ImageFile.builder()
+        .originUrl("origin")
+        .thumbnailUrl("thumbnail")
+        .build();
+    em.persist(imageFile2);
+
+    language = Language.builder()
+        .locale(Locale.KOREAN)
+        .dateFormat("yy-mm-dd")
+        .build();
+    em.persist(language);
+
+    member1 = Member.builder()
+        .email("test@naver.com")
+        .provider(Provider.KAKAO)
+        .providerId(123456789L)
+        .nickname("nickname1")
+        .language(language)
+        .profileImageFile(imageFile1)
+        .build();
+    em.persist(member1);
+
+    member2 = Member.builder()
+        .email("test2@naver.com")
+        .provider(Provider.KAKAO)
+        .providerId(1234567890L)
+        .nickname("nickname2")
+        .language(language)
+        .profileImageFile(imageFile2)
+        .build();
+    em.persist(member2);
+
+    experience = Experience.builder()
+        .imageFile(imageFile1)
+        .build();
+    em.persist(experience);
+
+    category = Category.builder()
+        .content("EXPERIENCE")
+        .build();
+    em.persist(category);
+  }
+
+  @Test
+  void toggleLikeStatusTest() {
+    /**
+     * WHEN
+     *
+     * member1 : toggleLikeStatus 2번 적용
+     * member2 : toggleLikeStatus 1번 적용
+     */
+    experienceService.toggleLikeStatus(member1, experience.getId());
+    experienceService.toggleLikeStatus(member1, experience.getId());
+
+    experienceService.toggleLikeStatus(member2, experience.getId());
+
+    Optional<Favorite> favoriteOptional1 =
+        favoriteRepository.findByMemberAndCategoryAndPostId(member1, category, experience.getId());
+    Optional<Favorite> favoriteOptional2 =
+        favoriteRepository.findByMemberAndCategoryAndPostId(member2, category, experience.getId());
+
+    /**
+     * THEN
+     *
+     * member1 = 좋아요 X, member2 = 좋아요
+     */
+    assertThat(favoriteOptional1.isPresent()).isFalse();
+    assertThat(favoriteOptional2.isPresent()).isTrue();
+  }
 }
