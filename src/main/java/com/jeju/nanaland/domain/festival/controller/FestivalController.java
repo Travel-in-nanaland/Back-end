a0dc2bf5c9a652d45d87b8dee04dac053f7b0971
seokhee <skdlzl753@naver.com>
2024-04-19 12:46:57 +0900

    [#54] feat: experience swagger

---
+++ src/main/java/com/jeju/nanaland/domain/festival/controller/FestivalController.java
@@ -0,0 +1,14 @@
+package com.jeju.nanaland.domain.festival.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/festival")
+@Slf4j
+public class FestivalController {
+
+}
