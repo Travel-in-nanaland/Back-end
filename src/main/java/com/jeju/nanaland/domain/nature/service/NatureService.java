7c6fbd7ac3132248a24ab6b34f9a74a73e902ae6
seokhee <skdlzl753@naver.com>
2024-04-19 13:50:56 +0900

    [#54] feat: nature 좋아요

--- src/main/java/com/jeju/nanaland/domain/nature/service/NatureService.java
+++ src/main/java/com/jeju/nanaland/domain/nature/service/NatureService.java
@@ -3,6 +3,8 @@ package com.jeju.nanaland.domain.nature.service;
 import com.jeju.nanaland.domain.common.data.CategoryContent;
 import com.jeju.nanaland.domain.favorite.service.FavoriteService;
 import com.jeju.nanaland.domain.member.entity.Member;
+import com.jeju.nanaland.domain.nature.repository.NatureRepository;
+import com.jeju.nanaland.global.exception.BadRequestException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -13,10 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class NatureService {
 
+  private final NatureRepository natureRepository;
   private final FavoriteService favoriteService;
 
   @Transactional
   public String toggleLikeStatus(Member member, Long postId) {
+    natureRepository.findById(postId)
+        .orElseThrow(() -> new BadRequestException("해당 id의 7대자연 게시물이 존재하지 않습니다."));
+
     return favoriteService.toggleLikeStatus(member, CategoryContent.NATURE, postId);
   }
 }
