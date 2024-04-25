b04c13f98af7ac00d6dccda0885df67563e7242d
seokhee <skdlzl753@naver.com>
2024-04-26 00:47:56 +0900

    [#96] refactor: 전통시장 반환에 좋아요 추가

--- src/main/java/com/jeju/nanaland/domain/market/service/MarketService.java
+++ src/main/java/com/jeju/nanaland/domain/market/service/MarketService.java
@@ -1,13 +1,17 @@
 package com.jeju.nanaland.domain.market.service;
 
+import static com.jeju.nanaland.domain.common.data.CategoryContent.MARKET;
+
 import com.jeju.nanaland.domain.common.entity.Locale;
+import com.jeju.nanaland.domain.favorite.service.FavoriteService;
 import com.jeju.nanaland.domain.market.dto.MarketCompositeDto;
 import com.jeju.nanaland.domain.market.dto.MarketResponse;
 import com.jeju.nanaland.domain.market.dto.MarketResponse.MarketThumbnail;
+import com.jeju.nanaland.domain.market.dto.MarketResponse.MarketThumbnailDto;
 import com.jeju.nanaland.domain.market.repository.MarketRepository;
 import com.jeju.nanaland.domain.member.dto.MemberResponse.MemberInfoDto;
-import com.jeju.nanaland.global.exception.BadRequestException;
-import java.util.ArrayList;
+import com.jeju.nanaland.global.exception.ErrorCode;
+import com.jeju.nanaland.global.exception.NotFoundException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,6 +26,7 @@ import org.springframework.stereotype.Service;
 public class MarketService {
 
   private final MarketRepository marketRepository;
+  private final FavoriteService favoriteService;
 
   public MarketResponse.MarketThumbnailDto getMarketList(MemberInfoDto memberInfoDto,
       List<String> addressFilterList,
@@ -33,40 +38,49 @@ public class MarketService {
     Page<MarketCompositeDto> marketCompositeDtoPage = marketRepository.findMarketThumbnails(locale,
         addressFilterList, pageable);
 
-    List<MarketThumbnail> data = new ArrayList<>();
-    for (MarketCompositeDto marketCompositeDto : marketCompositeDtoPage) {
-      data.add(MarketThumbnail.builder()
-          .id(marketCompositeDto.getId())
-          .title(marketCompositeDto.getTitle())
-          .thumbnailUrl(marketCompositeDto.getThumbnailUrl())
-          .addressTag(marketCompositeDto.getAddressTag())
-          .build());
-    }
+    List<Long> favoriteIds = favoriteService.getMemberFavoritePostIds(
+        memberInfoDto.getMember(), MARKET);
 
-    return MarketResponse.MarketThumbnailDto.builder()
+    List<MarketThumbnail> data = marketCompositeDtoPage.getContent()
+        .stream().map(marketCompositeDto ->
+            MarketThumbnail.builder()
+                .id(marketCompositeDto.getId())
+                .title(marketCompositeDto.getTitle())
+                .thumbnailUrl(marketCompositeDto.getThumbnailUrl())
+                .addressTag(marketCompositeDto.getAddressTag())
+                .isFavorite(favoriteIds.contains(marketCompositeDto.getId()))
+                .build()).toList();
+
+    return MarketThumbnailDto.builder()
         .totalElements(marketCompositeDtoPage.getTotalElements())
         .data(data)
         .build();
   }
 
   public MarketResponse.MarketDetailDto getMarketDetail(MemberInfoDto memberInfoDto, Long id) {
-    marketRepository.findById(id)
-        .orElseThrow(() -> new BadRequestException("해당 id의 전통시장 게시물이 존재하지 않습니다."));
 
-    Locale locale = memberInfoDto.getLanguage().getLocale();
-    MarketCompositeDto resultDto = marketRepository.findCompositeDtoById(id, locale);
+    MarketCompositeDto marketCompositeDto = marketRepository.findCompositeDtoById(id,
+        memberInfoDto.getLanguage().getLocale());
+
+    if (marketCompositeDto == null) {
+      throw new NotFoundException(ErrorCode.NOT_FOUND_EXCEPTION.getMessage());
+    }
+
+    boolean isPostInFavorite = favoriteService.isPostInFavorite(memberInfoDto.getMember(), MARKET,
+        id);
 
     return MarketResponse.MarketDetailDto.builder()
-        .id(resultDto.getId())
-        .title(resultDto.getTitle())
-        .originUrl(resultDto.getOriginUrl())
-        .content(resultDto.getContent())
-        .address(resultDto.getAddress())
-        .addressTag(resultDto.getAddressTag())
-        .contact(resultDto.getContact())
-        .homepage(resultDto.getHomepage())
-        .time(resultDto.getTime())
-        .amenity(resultDto.getAmenity())
+        .id(marketCompositeDto.getId())
+        .title(marketCompositeDto.getTitle())
+        .originUrl(marketCompositeDto.getOriginUrl())
+        .content(marketCompositeDto.getContent())
+        .address(marketCompositeDto.getAddress())
+        .addressTag(marketCompositeDto.getAddressTag())
+        .contact(marketCompositeDto.getContact())
+        .homepage(marketCompositeDto.getHomepage())
+        .time(marketCompositeDto.getTime())
+        .amenity(marketCompositeDto.getAmenity())
+        .isFavorite(isPostInFavorite)
         .build();
   }
 }
