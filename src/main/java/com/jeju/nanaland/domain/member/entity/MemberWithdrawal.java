d66060a75fd84a43132554401d0304906d39d0e3
jyajoo <jyajoo1020@gmail.com>
2024-05-09 23:51:59 +0900

    [#150] feat: MemberWithdrawal(회원 탈퇴) 추가

---
+++ src/main/java/com/jeju/nanaland/domain/member/entity/MemberWithdrawal.java
@@ -0,0 +1,37 @@
+package com.jeju.nanaland.domain.member.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberWithdrawal {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotNull
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id", nullable = false)
+  private Member member;
+
+  @NotNull
+  @Enumerated(EnumType.STRING)
+  private WithdrawalType withdrawalType;
+
+  private LocalDateTime withdrawalDate;
+}
