97c4e270639855f046bf007fa8c70c1897742dee
teho <xogh8755@naver.com>
2024-05-19 17:10:34 +0900

    [#191] refactor: nanaTitle에서 imageFile삭제, nana에 imageFile 추가

--- src/main/java/com/jeju/nanaland/domain/nana/entity/Nana.java
+++ src/main/java/com/jeju/nanaland/domain/nana/entity/Nana.java
@@ -1,7 +1,12 @@
 package com.jeju.nanaland.domain.nana.entity;
 
 import com.jeju.nanaland.domain.common.entity.BaseEntity;
+import com.jeju.nanaland.domain.common.entity.ImageFile;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -16,10 +21,14 @@ public class Nana extends BaseEntity {
   @NotBlank
   private String version;
 
+  @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+  @JoinColumn(name = "image_file_id", nullable = false)
+  private ImageFile imageFile;
 
   @Builder
-  public Nana(String version) {
+  public Nana(String version, ImageFile imageFile) {
     this.version = version;
+    this.imageFile = imageFile;
   }
 }
 
