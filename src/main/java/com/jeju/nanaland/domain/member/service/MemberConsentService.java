d7692dd43db0f91a76975d836b96fa019c5b4ec0
jyajoo <jyajoo1020@gmail.com>
2024-05-23 23:01:36 +0900

    [#202] feat: 회원 가입 시, 모든 이용약관이 추가될 수 있도록 로직 변경

--- src/main/java/com/jeju/nanaland/domain/member/service/MemberConsentService.java
+++ src/main/java/com/jeju/nanaland/domain/member/service/MemberConsentService.java
@@ -8,7 +8,9 @@ import com.jeju.nanaland.domain.member.entity.MemberConsent;
 import com.jeju.nanaland.domain.member.entity.enums.ConsentType;
 import com.jeju.nanaland.domain.member.repository.MemberConsentRepository;
 import com.jeju.nanaland.domain.member.repository.MemberRepository;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -24,14 +26,22 @@ public class MemberConsentService {
   private final MemberRepository memberRepository;
 
   public void createMemberConsents(Member member, List<ConsentItem> consentItems) {
-    List<MemberConsent> memberConsents = consentItems
-        .stream().map(consentItem ->
-            MemberConsent.builder()
-                .member(member)
-                .consentType(ConsentType.valueOf(consentItem.getConsentType()))
-                .consent(consentItem.getConsent())
-                .build())
-        .collect(Collectors.toList());
+    Map<ConsentType, Boolean> consentItemMap = consentItems.stream()
+        .collect(Collectors.toMap(
+            consentItem -> ConsentType.valueOf(consentItem.getConsentType()),
+            ConsentItem::getConsent
+        ));
+
+    List<MemberConsent> memberConsents = Arrays.stream(ConsentType.values())
+        .map(consentType -> {
+          Boolean consent = consentItemMap.get(consentType);
+          return MemberConsent.builder()
+              .member(member)
+              .consentType(consentType)
+              .consent(consent != null && consent)
+              .build();
+        }).collect(Collectors.toList());
+
     memberConsentRepository.saveAll(memberConsents);
   }
 
