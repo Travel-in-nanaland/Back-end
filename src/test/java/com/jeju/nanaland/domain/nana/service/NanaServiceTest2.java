f986eddd06f67c814c8262c69b9925a4043410b0
teho <xogh8755@naver.com>
2024-05-23 15:12:37 +0900

    [#173] teste: 초기 데이터 메서드 분류

--- src/test/java/com/jeju/nanaland/domain/nana/service/NanaServiceTest2.java
+++ src/test/java/com/jeju/nanaland/domain/nana/service/NanaServiceTest2.java
@@ -25,6 +25,7 @@ import com.jeju.nanaland.domain.nana.entity.NanaTitle;
 import com.jeju.nanaland.domain.nana.repository.NanaContentRepository;
 import com.jeju.nanaland.domain.nana.repository.NanaRepository;
 import com.jeju.nanaland.domain.nana.repository.NanaTitleRepository;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -56,48 +57,94 @@ public class NanaServiceTest2 {
   @Test
   void getNanaDetail() {
     // Given
-    Language language = Language.builder()
-        .locale(Locale.KOREAN)
+    Language language = createLanguage(Locale.KOREAN);
+    ImageFile imageFile = createImageFile(1);
+    Member member = createMember(language, imageFile);
+    MemberInfoDto memberInfoDto = createMemberInfoDto(member, language);
+    Nana nana = createNana(1, imageFile);
+    NanaTitle nanaTitle = createNanaTitle(1, nana, language);
+    List<NanaContent> nanaContentList = createNanaContentList(nanaTitle);
+    List<NanaContentImage> nanaContentImage = createNanaContentImage(
+        List.of(createImageFile(1), createImageFile(2), createImageFile(3)), nana);
+    nana.updateNanaContentImageList(nanaContentImage);
+    Category category = createCategory(CategoryContent.NANA);
+
+    when(nanaRepository.findNanaById(anyLong())).thenReturn(Optional.of(nana));
+    when(nanaTitleRepository.findNanaTitleByNanaAndLanguage(nana, language)).thenReturn(
+        Optional.of(nanaTitle));
+    when(nanaContentRepository.findAllByNanaTitleOrderByNumber(nanaTitle)).thenReturn(
+        nanaContentList);
+    when(favoriteService.isPostInFavorite(memberInfoDto.getMember(), CategoryContent.NANA,
+        nanaTitle.getNana().getId())).thenReturn(true);
+    when(categoryRepository.findByContent(CategoryContent.NANA_CONTENT)).thenReturn(
+        Optional.of(category));
+
+    // When
+    NanaDetailDto nanaDetail = nanaService.getNanaDetail(memberInfoDto, 1L, false);
+    List<NanaDetail> nanaDetails = nanaDetail.getNanaDetails();
+
+    // Then
+    int[] numberList = {nanaDetails.get(0).number, nanaDetails.get(1).number,
+        nanaDetails.get(2).number};
+    Assertions.assertThat(numberList).containsSequence(1, 2, 3);
+  }
+
+  Language createLanguage(Locale locale) {
+    return Language.builder()
+        .locale(locale)
         .dateFormat("yyyy-MM-dd")
         .build();
-    ImageFile imageFile1 = ImageFile.builder()
-        .thumbnailUrl("thumbnail_url1")
-        .originUrl("origin_url1")
-        .build();
+  }
 
-    ImageFile imageFile2 = ImageFile.builder()
-        .thumbnailUrl("thumbnail_url2")
-        .originUrl("origin_url2")
+  Category createCategory(CategoryContent categoryContent) {
+    return Category.builder()
+        .content(categoryContent)
         .build();
+  }
 
-    ImageFile imageFile3 = ImageFile.builder()
-        .thumbnailUrl("thumbnail_url3")
-        .originUrl("origin_url3")
+  ImageFile createImageFile(int idx) {
+    return ImageFile.builder()
+        .thumbnailUrl("thumbnail_url" + idx)
+        .originUrl("origin_url" + idx)
         .build();
+  }
 
-    Member member = Member.builder()
+  Member createMember(Language language, ImageFile imageFile) {
+    return Member.builder()
         .email("test@naver.com")
         .provider(Provider.KAKAO)
         .providerId(String.valueOf(123456789L))
         .nickname("nickname1")
         .language(language)
-        .profileImageFile(imageFile1)
+        .profileImageFile(imageFile)
         .build();
+  }
 
-    MemberInfoDto memberInfoDto = MemberInfoDto.builder()
+  MemberInfoDto createMemberInfoDto(Member member, Language language) {
+    return MemberInfoDto.builder()
         .member(member)
         .language(language)
         .build();
-    Nana nana = Nana.builder()
-        .version("ver1")
-        .nanaTitleImageFile(imageFile1)
+  }
+
+  Nana createNana(int idx, ImageFile imageFile) {
+    return Nana.builder()
+        .version("ver" + idx)
+        .nanaTitleImageFile(imageFile)
         .build();
-    NanaTitle nanaTitle = NanaTitle.builder()
-        .notice("notice1")
+  }
+
+  NanaTitle createNanaTitle(int idx, Nana nana, Language language) {
+    return NanaTitle.builder()
+        .notice("notice" + idx)
         .language(language)
         .nana(nana)
         .build();
-    List<NanaContent> nanaContentList = List.of(NanaContent.builder()
+  }
+
+  //nanaContent 3개 생성
+  List<NanaContent> createNanaContentList(NanaTitle nanaTitle) {
+    return List.of(NanaContent.builder()
             .subTitle("subtitle1")
             .nanaTitle(nanaTitle)
             .content("content")
@@ -147,49 +194,118 @@ public class NanaServiceTest2 {
                     .infoType(InfoType.ADDRESS)
                     .build()))
             .build());
-
-    NanaContentImage nanaContentImage1 = NanaContentImage.builder()
-        .imageFile(imageFile1)
-        .nana(nana)
-        .number(1)
-        .build();
-
-    NanaContentImage nanaContentImage2 = NanaContentImage.builder()
-        .imageFile(imageFile2)
-        .nana(nana)
-        .number(2)
-        .build();
-
-    NanaContentImage nanaContentImage3 = NanaContentImage.builder()
-        .imageFile(imageFile3)
-        .nana(nana)
-        .number(3)
-        .build();
-
-    nana.updateNanaContentImageList(
-        List.of(nanaContentImage1, nanaContentImage2, nanaContentImage3));
-
-    Category category = Category.builder()
-        .content(CategoryContent.NANA_CONTENT)
-        .build();
-
-    when(nanaRepository.findNanaById(anyLong())).thenReturn(Optional.of(nana));
-    when(nanaTitleRepository.findNanaTitleByNanaAndLanguage(nana, language)).thenReturn(
-        Optional.of(nanaTitle));
-    when(nanaContentRepository.findAllByNanaTitleOrderByNumber(nanaTitle)).thenReturn(
-        nanaContentList);
-    when(favoriteService.isPostInFavorite(memberInfoDto.getMember(), CategoryContent.NANA,
-        nanaTitle.getNana().getId())).thenReturn(true);
-    when(categoryRepository.findByContent(CategoryContent.NANA_CONTENT)).thenReturn(
-        Optional.of(category));
-
-    // When
-    NanaDetailDto nanaDetail = nanaService.getNanaDetail(memberInfoDto, 1L, false);
-    List<NanaDetail> nanaDetails = nanaDetail.getNanaDetails();
-
-    // Then
-    int[] numberList = {nanaDetails.get(0).number, nanaDetails.get(1).number,
-        nanaDetails.get(2).number};
-    Assertions.assertThat(numberList).containsSequence(1, 2, 3);
   }
+
+  List<NanaContentImage> createNanaContentImage(List<ImageFile> imageFileList, Nana nana) {
+    List<NanaContentImage> nanaContentImageList = new ArrayList<>();
+    int i = 1;
+    for (ImageFile imageFile : imageFileList) {
+      nanaContentImageList.add(NanaContentImage.builder()
+          .imageFile(imageFile)
+          .nana(nana)
+          .number(i++)
+          .build());
+    }
+    return nanaContentImageList;
+  }
+
+//  void setData() {
+//    Member member = Member.builder()
+//        .email("test@naver.com")
+//        .provider(Provider.KAKAO)
+//        .providerId(String.valueOf(123456789L))
+//        .nickname("nickname1")
+//        .language(language)
+//        .profileImageFile(imageFile1)
+//        .build();
+//
+//    MemberInfoDto memberInfoDto = MemberInfoDto.builder()
+//        .member(member)
+//        .language(language)
+//        .build();
+//    Nana nana = Nana.builder()
+//        .version("ver1")
+//        .nanaTitleImageFile(imageFile1)
+//        .build();
+//    NanaTitle nanaTitle = NanaTitle.builder()
+//        .notice("notice1")
+//        .language(language)
+//        .nana(nana)
+//        .build();
+//    List<NanaContent> nanaContentList = List.of(NanaContent.builder()
+//            .subTitle("subtitle1")
+//            .nanaTitle(nanaTitle)
+//            .content("content")
+//            .number(1)
+//            .title("title")
+//            .infoList(Set.of(
+//                NanaAdditionalInfo.builder()
+//                    .description("description1")
+//                    .infoType(InfoType.ADDRESS)
+//                    .build()
+//                , NanaAdditionalInfo.builder()
+//                    .description("description2")
+//                    .infoType(InfoType.ADDRESS)
+//                    .build()))
+//            .build(),
+//
+//        NanaContent.builder()
+//            .subTitle("subtitle2")
+//            .nanaTitle(nanaTitle)
+//            .content("content2")
+//            .number(2)
+//            .title("title2")
+//            .infoList(Set.of(
+//                NanaAdditionalInfo.builder()
+//                    .description("description3")
+//                    .infoType(InfoType.ADDRESS)
+//                    .build()
+//                , NanaAdditionalInfo.builder()
+//                    .description("description4")
+//                    .infoType(InfoType.ADDRESS)
+//                    .build()))
+//            .build(),
+//
+//        NanaContent.builder()
+//            .subTitle("subtitle3")
+//            .nanaTitle(nanaTitle)
+//            .content("content3")
+//            .number(3)
+//            .title("title3")
+//            .infoList(Set.of(
+//                NanaAdditionalInfo.builder()
+//                    .description("description5")
+//                    .infoType(InfoType.ADDRESS)
+//                    .build()
+//                , NanaAdditionalInfo.builder()
+//                    .description("description6")
+//                    .infoType(InfoType.ADDRESS)
+//                    .build()))
+//            .build());
+//
+//    NanaContentImage nanaContentImage1 = NanaContentImage.builder()
+//        .imageFile(imageFile1)
+//        .nana(nana)
+//        .number(1)
+//        .build();
+//
+//    NanaContentImage nanaContentImage2 = NanaContentImage.builder()
+//        .imageFile(imageFile2)
+//        .nana(nana)
+//        .number(2)
+//        .build();
+//
+//    NanaContentImage nanaContentImage3 = NanaContentImage.builder()
+//        .imageFile(imageFile3)
+//        .nana(nana)
+//        .number(3)
+//        .build();
+//
+//    nana.updateNanaContentImageList(
+//        List.of(nanaContentImage1, nanaContentImage2, nanaContentImage3));
+//
+//    Category category = Category.builder()
+//        .content(CategoryContent.NANA_CONTENT)
+//        .build();
+//  }
 }
