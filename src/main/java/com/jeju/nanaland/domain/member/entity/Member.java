ec31307b76ead59a964cbe6a131add3181e480db
seokhee <skdlzl753@naver.com>
2024-05-31 14:15:10 +0900

    [#222] feat: 이메일 정규식 적용

--- src/main/java/com/jeju/nanaland/domain/member/entity/Member.java
+++ src/main/java/com/jeju/nanaland/domain/member/entity/Member.java
@@ -22,7 +22,6 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -49,9 +48,6 @@ public class Member extends BaseEntity {
   private Language language;
 
   @NotBlank
-  @Pattern(
-      regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\\\.[a-zA-Z]{2,}\\$",
-      message = "이메일 형식이 올바르지 않습니다.")
   @Column(nullable = false)
   private String email;
 
