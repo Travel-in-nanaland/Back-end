6fbac98aeec3adc6017ac42460b703c553aacac5
hseoky desktop <skdlzl753@naver.com>
2024-06-20 15:02:34 +0900

    [#254] feat: 전통시장 썸네일 리스트 조회 테스트

--- src/test/java/com/jeju/nanaland/domain/market/repository/MarketRepositoryTest.java
+++ src/test/java/com/jeju/nanaland/domain/market/repository/MarketRepositoryTest.java
@@ -1,12 +1,24 @@
 package com.jeju.nanaland.domain.market.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.jeju.nanaland.config.TestConfig;
+import com.jeju.nanaland.domain.common.entity.ImageFile;
+import com.jeju.nanaland.domain.common.entity.Language;
 import com.jeju.nanaland.domain.common.entity.Locale;
+import com.jeju.nanaland.domain.market.dto.MarketResponse.MarketThumbnail;
+import com.jeju.nanaland.domain.market.entity.Market;
+import com.jeju.nanaland.domain.market.entity.MarketTrans;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
@@ -17,10 +29,76 @@ class MarketRepositoryTest {
   @Autowired
   MarketRepository marketRepository;
 
+  @Autowired
+  TestEntityManager em;
+
   @Test
   @DisplayName("전통시장 검색")
   void searchMarketTest() {
     Pageable pageable = PageRequest.of(0, 12);
     marketRepository.searchCompositeDtoByKeyword("쇼핑", Locale.KOREAN, pageable);
+  }
+
+  @Test
+  @DisplayName("전통시장 썸네일 조회")
+  void findMarketThumbnailsTest() {
+    // given
+    Language korean = initKoreanLanguage();
+    List<Market> marketList = getMarketList(korean);
+    for (Market market : marketList) {
+      System.out.println(market.getCreatedAt());
+    }
+
+    Locale locale = Locale.KOREAN;
+    List<String> addressFilter = Arrays.asList("제주시");
+    Pageable pageable = PageRequest.of(0, 2);
+
+    // when
+    Page<MarketThumbnail> marketThumbnails = marketRepository.findMarketThumbnails(locale,
+        addressFilter, pageable);
+    List<MarketThumbnail> thumbnails = marketThumbnails.getContent();
+
+    // then
+    assertThat(thumbnails).hasSize(2);
+    assertThat(thumbnails.get(0)).extracting("title").isEqualTo("market title 10");
+    assertThat(thumbnails.get(1)).extracting("title").isEqualTo("market title 9");
+  }
+
+  // KOREAN 언어 정보 초기 설정
+  Language initKoreanLanguage() {
+    Language korean = Language.builder()
+        .locale(Locale.KOREAN)
+        .dateFormat("yyyy-mm-dd")
+        .build();
+    em.persist(korean);
+    return korean;
+  }
+
+  // 언어: KOREAN, 전통시장 데이터 10개 생성
+  List<Market> getMarketList(Language language) {
+    List<Market> marketList = new ArrayList<>();
+    for (int i = 1; i < 11; i++) {
+      ImageFile imageFile = ImageFile.builder()
+          .originUrl("originUrl" + i)
+          .thumbnailUrl("thumbnailUrl" + i)
+          .build();
+      em.persist(imageFile);
+      Market market = Market.builder()
+          .firstImageFile(imageFile)
+          .priority(0L)
+          .build();
+      em.persistAndFlush(market);
+      MarketTrans marketTrans1 = MarketTrans.builder()
+          .market(market)
+          .title("market title " + i)
+          .language(language)
+          .addressTag("제주시")
+          .build();
+      em.persist(marketTrans1);
+
+      marketList.add(market);
+    }
+
+    return marketList;
   }
 }
