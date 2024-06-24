f330f9e7e8d0c2e319520359b2d7722dbb0fadb5
teho <xogh8755@naver.com>
2024-06-24 15:45:09 +0900

    [#253] refactor: NanalandApplicationsTest 재생성, contextLoads 재생성

--- src/test/java/com/jeju/nanaland/NanalandApplicationTests.java
+++ src/test/java/com/jeju/nanaland/NanalandApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.jeju.nanaland;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class NanalandApplicationTests {
-//
-//  @Test
-//  void contextLoads() {
-//  }
-//
-//}
+package com.jeju.nanaland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanalandApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
