bd9adf1aae884d335167bc93185a1c368964e85a
teho <xogh8755@naver.com>
2024-06-25 01:18:38 +0900

    [#253] refactor: contextLoads() 주석 해제, 테스트코드 병렬 처리 X

--- src/test/java/com/jeju/nanaland/NanalandApplicationTests.java
+++ src/test/java/com/jeju/nanaland/NanalandApplicationTests.java
@@ -1,12 +1,13 @@
 package com.jeju.nanaland;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NanalandApplicationTests {
 
-//  @Test
-//  void contextLoads() {
-//  }
+  @Test
+  void contextLoads() {
+  }
 
 }
