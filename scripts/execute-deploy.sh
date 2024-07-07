196b81a6c053bca20edcbe18d5836f4aa7743a36
hseoky desktop <skdlzl753@naver.com>
2024-07-07 22:59:59 +0900

    [#284] hotfix: nginx deploy 설정

--- scripts/execute-deploy.sh
+++ scripts/execute-deploy.sh
@@ -3,4 +3,4 @@ DEPLOY_SH=/home/ubuntu/Back-end/scripts/nginx-deploy.sh
 DEPLOY_LOG=/home/ubuntu/Back-end/deploy.log
 DEPLOY_ERROR_LOG=/home/ubuntu/Back-end/deploy-error.log
 
-sh $DEPLOY_SH > $DEPLOY_LOG 2> $DEPLOY_ERROR_LOG &
+sudo sh $DEPLOY_SH > $DEPLOY_LOG 2> $DEPLOY_ERROR_LOG &
