71ae14f3c275a06a781831808d85cdf897bf41c0
hseoky desktop <skdlzl753@naver.com>
2024-07-23 20:59:09 +0900

    [#312] feat: 정보 수정 제안 다중이미지 테스트

--- src/test/java/com/jeju/nanaland/domain/report/service/ReportServiceTest.java
+++ src/test/java/com/jeju/nanaland/domain/report/service/ReportServiceTest.java
@@ -2,11 +2,13 @@ package com.jeju.nanaland.domain.report.service;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
 import com.jeju.nanaland.domain.common.data.Category;
 import com.jeju.nanaland.domain.common.data.Language;
+import com.jeju.nanaland.domain.common.entity.ImageFile;
 import com.jeju.nanaland.domain.common.service.ImageFileService;
 import com.jeju.nanaland.domain.experience.repository.ExperienceRepository;
 import com.jeju.nanaland.domain.festival.repository.FestivalRepository;
@@ -19,6 +21,7 @@ import com.jeju.nanaland.domain.nature.repository.NatureRepository;
 import com.jeju.nanaland.domain.report.dto.ReportRequest.InfoFixDto;
 import com.jeju.nanaland.domain.report.entity.FixType;
 import com.jeju.nanaland.domain.report.entity.InfoFixReport;
+import com.jeju.nanaland.domain.report.entity.InfoFixReportImageFile;
 import com.jeju.nanaland.domain.report.repository.InfoFixReportImageFileRepository;
 import com.jeju.nanaland.domain.report.repository.InfoFixReportRepository;
 import com.jeju.nanaland.domain.restaurant.repository.RestaurantRepository;
@@ -26,6 +29,7 @@ import com.jeju.nanaland.global.exception.BadRequestException;
 import com.jeju.nanaland.global.exception.NotFoundException;
 import jakarta.mail.internet.MimeMessage;
 import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -37,6 +41,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.multipart.MultipartFile;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
 @ExtendWith(MockitoExtension.class)
@@ -125,13 +130,16 @@ class ReportServiceTest {
 
     doReturn(NatureCompositeDto.builder().build()).when(natureRepository)
         .findCompositeDtoById(1L, Language.KOREAN);
+    doReturn(mock(ImageFile.class)).when(imageFileService)
+        .uploadAndSaveImageFile(any(MultipartFile.class), eq(false), eq("/info_fix_report_images"));
+    doReturn(null).when(infoFixReportImageFileRepository).save(any(InfoFixReportImageFile.class));
     doReturn(mock(MimeMessage.class)).when(javaMailSender).createMimeMessage();
     doReturn("nanaland.jeju@gmail.com").when(env).getProperty("spring.mail.username");
     doReturn(null).when(infoFixReportRepository).save(any(InfoFixReport.class));
 
     // when
     // then
-    reportService.postInfoFixReport(memberInfoDto, infoFixDto, new ArrayList<>());
+    reportService.postInfoFixReport(memberInfoDto, infoFixDto, List.of(mock(MultipartFile.class)));
   }
 
 
