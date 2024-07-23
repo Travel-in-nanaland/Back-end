3f40e24311cf8773a71ff8aaa2765ecc65496608
hseoky desktop <skdlzl753@naver.com>
2024-07-23 20:49:26 +0900

    [#312] feat: 정보 수정 제안 다중이미지 테스트

--- src/main/java/com/jeju/nanaland/domain/report/dto/ReportRequest.java
+++ src/main/java/com/jeju/nanaland/domain/report/dto/ReportRequest.java
@@ -7,12 +7,16 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 public class ReportRequest {
 
   @Data
   @Schema(description = "정보 수정 요청 Dto")
+  @AllArgsConstructor
+  @Builder
   public static class InfoFixDto {
 
     @NotNull
--- src/test/java/com/jeju/nanaland/domain/report/service/ReportServiceTest.java
+++ src/test/java/com/jeju/nanaland/domain/report/service/ReportServiceTest.java
@@ -1,123 +1,149 @@
 package com.jeju.nanaland.domain.report.service;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
 import com.jeju.nanaland.domain.common.data.Category;
 import com.jeju.nanaland.domain.common.data.Language;
-import com.jeju.nanaland.domain.common.entity.ImageFile;
+import com.jeju.nanaland.domain.common.service.ImageFileService;
+import com.jeju.nanaland.domain.experience.repository.ExperienceRepository;
+import com.jeju.nanaland.domain.festival.repository.FestivalRepository;
+import com.jeju.nanaland.domain.market.repository.MarketRepository;
 import com.jeju.nanaland.domain.member.dto.MemberResponse.MemberInfoDto;
 import com.jeju.nanaland.domain.member.entity.Member;
-import com.jeju.nanaland.domain.member.entity.enums.Provider;
 import com.jeju.nanaland.domain.member.entity.enums.TravelType;
-import com.jeju.nanaland.domain.nana.entity.Nana;
-import com.jeju.nanaland.domain.nana.entity.NanaTitle;
+import com.jeju.nanaland.domain.nature.dto.NatureCompositeDto;
+import com.jeju.nanaland.domain.nature.repository.NatureRepository;
 import com.jeju.nanaland.domain.report.dto.ReportRequest.InfoFixDto;
 import com.jeju.nanaland.domain.report.entity.FixType;
+import com.jeju.nanaland.domain.report.entity.InfoFixReport;
+import com.jeju.nanaland.domain.report.repository.InfoFixReportImageFileRepository;
+import com.jeju.nanaland.domain.report.repository.InfoFixReportRepository;
+import com.jeju.nanaland.domain.restaurant.repository.RestaurantRepository;
 import com.jeju.nanaland.global.exception.BadRequestException;
 import com.jeju.nanaland.global.exception.NotFoundException;
-import jakarta.persistence.EntityManager;
-import org.junit.jupiter.api.BeforeEach;
+import jakarta.mail.internet.MimeMessage;
+import java.util.ArrayList;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.EnumSource.Mode;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.env.Environment;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
-@SpringBootTest
-@Transactional
+@ExtendWith(MockitoExtension.class)
 class ReportServiceTest {
 
-  @Autowired
-  EntityManager em;
-  @Autowired
+  @InjectMocks
   ReportService reportService;
 
-  ImageFile imageFile1;
-  Language language;
-  Member member;
-  MemberInfoDto memberInfoDto;
-
-  @BeforeEach
-  void init() {
-    // imageFile
-    imageFile1 = ImageFile.builder()
-        .originUrl("origin1")
-        .thumbnailUrl("thumbnail1")
-        .build();
-    em.persist(imageFile1);
-
-    // member
-    member = Member.builder()
-        .email("test@naver.com")
-        .provider(Provider.KAKAO)
-        .providerId("123456789")
-        .nickname("nickname1")
-        .language(Language.KOREAN)
-        .profileImageFile(imageFile1)
-        .travelType(TravelType.NONE)
-        .build();
-    em.persist(member);
-
-    // memberInfoDto
-    memberInfoDto = MemberInfoDto.builder()
-        .language(Language.KOREAN)
-        .member(member)
-        .build();
-  }
+  @Mock
+  InfoFixReportRepository infoFixReportRepository;
+  @Mock
+  InfoFixReportImageFileRepository infoFixReportImageFileRepository;
+  @Mock
+  NatureRepository natureRepository;
+  @Mock
+  MarketRepository marketRepository;
+  @Mock
+  FestivalRepository festivalRepository;
+  @Mock
+  ExperienceRepository experienceRepository;
+  @Mock
+  RestaurantRepository restaurantRepository;
+  @Mock
+  ImageFileService imageFileService;
+  @Mock
+  Environment env;
+  @Mock
+  JavaMailSender javaMailSender;
+  @Mock
+  SpringTemplateEngine templateEngine;
 
   @Test
+  @DisplayName("없는 게시물에 대한 요청")
   void fixInfoReportWithWrongPost() {
-    /**
-     * GIVEN
-     */
-    InfoFixDto infoFixDto = new InfoFixDto();
-    infoFixDto.setFixType(FixType.TIME.name());
-    infoFixDto.setPostId(-1L);
-    infoFixDto.setCategory(Category.FESTIVAL.name());
-    infoFixDto.setContent("content");
-    infoFixDto.setEmail("test@naver.com");
+    // given
+    MemberInfoDto memberInfoDto = createMemberInfoDto(Language.KOREAN, TravelType.NONE);
+    InfoFixDto infoFixDto = InfoFixDto.builder()
+        .fixType(FixType.CONTACT_OR_HOMEPAGE.name())
+        .postId(1L)
+        .email("test@naver.com")
+        .category(Category.MARKET.name())
+        .content("수정 내용")
+        .build();
+    doReturn(null).when(marketRepository).findCompositeDtoById(1L, Language.KOREAN);
 
-    /**
-     * WHEN
-     * THEN
-     */
+    // when
+    // then
     assertThatThrownBy(
-        () -> reportService.postInfoFixReport(memberInfoDto, infoFixDto, null)
+        () -> reportService.postInfoFixReport(memberInfoDto, infoFixDto, new ArrayList<>())
     ).isInstanceOf(NotFoundException.class);
   }
 
-  @Test
-  void fixInfoReportWithNanaTest() {
-    /**
-     * GIVEN
-     */
-    Nana nana = Nana.builder()
-        .version("1")
-        .firstImageFile(imageFile1)
-        .priority(0L)
+  @ParameterizedTest
+  @DisplayName("나나스픽 정보 수정 제안 요청")
+  @EnumSource(value = Category.class, names = {"NANA", "NANA_CONTENT"}, mode = Mode.INCLUDE)
+  void fixInfoReportWithNanaTest(Category category) {
+    // given
+    MemberInfoDto memberInfoDto = createMemberInfoDto(Language.KOREAN, TravelType.NONE);
+    InfoFixDto infoFixDto = InfoFixDto.builder()
+        .fixType(FixType.CONTACT_OR_HOMEPAGE.name())
+        .postId(1L)
+        .email("test@naver.com")
+        .category(category.name())
+        .content("수정 내용")
         .build();
-    em.persist(nana);
 
-    NanaTitle nanaTitle = NanaTitle.builder()
-        .heading("heading")
-        .subHeading("subHeading")
-        .language(Language.KOREAN)
-        .nana(nana)
-        .build();
-    em.persist(nanaTitle);
-
-    InfoFixDto infoFixDto = new InfoFixDto();
-    infoFixDto.setFixType(FixType.CONTACT_OR_HOMEPAGE.name());
-    infoFixDto.setPostId(nana.getId());
-    infoFixDto.setCategory(Category.NANA.name());
-    infoFixDto.setContent("content");
-    infoFixDto.setEmail("test@naver.com");
-
-    /**
-     * WHEN
-     * THEN
-     */
+    // when
+    // then
     assertThatThrownBy(
-        () -> reportService.postInfoFixReport(memberInfoDto, infoFixDto, null)
+        () -> reportService.postInfoFixReport(memberInfoDto, infoFixDto, new ArrayList<>())
     ).isInstanceOf(BadRequestException.class);
+  }
+
+  @Test
+  @DisplayName("정보 수정 제안 요청")
+  void fixInfoReportTest() {
+    // given
+    MemberInfoDto memberInfoDto = createMemberInfoDto(Language.KOREAN, TravelType.NONE);
+    InfoFixDto infoFixDto = InfoFixDto.builder()
+        .fixType(FixType.CONTACT_OR_HOMEPAGE.name())
+        .postId(1L)
+        .email("test@naver.com")
+        .category(Category.NATURE.name())
+        .content("수정 내용")
+        .build();
+
+    doReturn(NatureCompositeDto.builder().build()).when(natureRepository)
+        .findCompositeDtoById(1L, Language.KOREAN);
+    doReturn(mock(MimeMessage.class)).when(javaMailSender).createMimeMessage();
+    doReturn("nanaland.jeju@gmail.com").when(env).getProperty("spring.mail.username");
+    doReturn(null).when(infoFixReportRepository).save(any(InfoFixReport.class));
+
+    // when
+    // then
+    reportService.postInfoFixReport(memberInfoDto, infoFixDto, new ArrayList<>());
+  }
+
+
+  private MemberInfoDto createMemberInfoDto(Language language, TravelType travelType) {
+    Member member = Member.builder()
+        .language(language)
+        .travelType(travelType)
+        .build();
+
+    return MemberInfoDto.builder()
+        .member(member)
+        .language(language)
+        .build();
   }
 }
