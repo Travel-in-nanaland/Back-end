ac20d61daf7ff5d1b97652aefa8c803adbcdd864
hseoky desktop <skdlzl753@naver.com>
2024-07-29 16:46:40 +0900

    [#319] feat: notification swagger

--- src/main/java/com/jeju/nanaland/domain/notification/controller/NotificationController.java
+++ src/main/java/com/jeju/nanaland/domain/notification/controller/NotificationController.java
@@ -5,6 +5,10 @@ import static com.jeju.nanaland.global.exception.SuccessCode.SEND_NOTIFICATION_S
 import com.jeju.nanaland.domain.notification.data.NotificationRequest;
 import com.jeju.nanaland.domain.notification.service.NotificationService;
 import com.jeju.nanaland.global.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +27,14 @@ public class NotificationController {
 
   private final NotificationService notificationService;
 
+  @Operation(
+      summary = "알림 전체 전송",
+      description = "모든 유저에게 알림 전송")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "성공"),
+      @ApiResponse(responseCode = "401", description = "accessToken이 유효하지 않은 경우", content = @Content),
+      @ApiResponse(responseCode = "500", description = "FCM을 통한 알림 전송 실패")
+  })
   @PostMapping("/send/all")
   public BaseResponse<String> sendNotificationToAllMember(
       @RequestBody @Valid NotificationRequest.FcmMessageDto fcmMessageDto) {
@@ -31,6 +43,14 @@ public class NotificationController {
     return BaseResponse.success(SEND_NOTIFICATION_SUCCESS);
   }
 
+  @Operation(
+      summary = "알림 개별 전송",
+      description = "FCM 토큰을 이용하여 특정 유저에게 알림 전송")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "성공"),
+      @ApiResponse(responseCode = "401", description = "accessToken이 유효하지 않은 경우", content = @Content),
+      @ApiResponse(responseCode = "500", description = "FCM을 통한 알림 전송 실패")
+  })
   @PostMapping("/send")
   public BaseResponse<String> sendNotificationToAllMember(
       @RequestBody @Valid NotificationRequest.FcmMessageToTargetDto reqDto) {
