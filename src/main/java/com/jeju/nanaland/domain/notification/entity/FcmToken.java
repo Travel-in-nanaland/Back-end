4969641f65c969f0058d614206405c05308ac54b
hseoky desktop <skdlzl753@naver.com>
2024-08-02 21:12:40 +0900

    [#319] feat: FcmToken 엔티티 Member 연관관계 추가

--- src/main/java/com/jeju/nanaland/domain/notification/entity/FcmToken.java
+++ src/main/java/com/jeju/nanaland/domain/notification/entity/FcmToken.java
@@ -1,9 +1,12 @@
 package com.jeju.nanaland.domain.notification.entity;
 
+import com.jeju.nanaland.domain.member.entity.Member;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -17,6 +20,10 @@ public class FcmToken {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "member_id", nullable = false)
+  private Member member;
 
   @NotNull
   private String token;
