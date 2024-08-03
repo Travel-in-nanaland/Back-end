28815178e1313bf823210b8f33ec04a075422bea
hseoky desktop <skdlzl753@naver.com>
2024-08-03 23:13:47 +0900

    [#319] feat: FcmToken 엔티티 타임스탬프 필드 추가

--- src/main/java/com/jeju/nanaland/domain/notification/entity/FcmToken.java
+++ src/main/java/com/jeju/nanaland/domain/notification/entity/FcmToken.java
@@ -1,11 +1,14 @@
 package com.jeju.nanaland.domain.notification.entity;
 
-import com.jeju.nanaland.domain.common.entity.BaseEntity;
 import com.jeju.nanaland.domain.member.entity.Member;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +16,11 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class FcmToken extends BaseEntity {
+public class FcmToken {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
   @ManyToOne
   @JoinColumn(name = "member_id", nullable = false)
@@ -21,4 +28,11 @@ public class FcmToken extends BaseEntity {
 
   @NotNull
   private String token;
+
+  @NotNull
+  private LocalDateTime timestamp;
+
+  public void updateTimestamp() {
+    timestamp = LocalDateTime.now();
+  }
 }
