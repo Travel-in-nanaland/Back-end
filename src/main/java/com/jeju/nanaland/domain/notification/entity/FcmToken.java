31f0f47dbb44af5abfe5185320627b5ec7de5f6e
hseoky desktop <skdlzl753@naver.com>
2024-08-03 19:36:34 +0900

    [#319] feat: FcmToken 엔티티 타임스탬프 추가

--- src/main/java/com/jeju/nanaland/domain/notification/entity/FcmToken.java
+++ src/main/java/com/jeju/nanaland/domain/notification/entity/FcmToken.java
@@ -1,10 +1,8 @@
 package com.jeju.nanaland.domain.notification.entity;
 
+import com.jeju.nanaland.domain.common.entity.BaseEntity;
 import com.jeju.nanaland.domain.member.entity.Member;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
@@ -15,11 +13,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class FcmToken {
-
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+public class FcmToken extends BaseEntity {
 
   @ManyToOne
   @JoinColumn(name = "member_id", nullable = false)
