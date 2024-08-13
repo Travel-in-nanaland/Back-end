0397c31315a2c8ea4e444bbbbae83a1a041ff7d4
seokhee <skdlzl753@naver.com>
2024-08-13 13:46:20 +0900

    [#319] feat: MemberNotification memberId nullable false

--- src/main/java/com/jeju/nanaland/domain/notification/entity/MemberNotification.java
+++ src/main/java/com/jeju/nanaland/domain/notification/entity/MemberNotification.java
@@ -1,5 +1,6 @@
 package com.jeju.nanaland.domain.notification.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,6 +24,7 @@ public class MemberNotification {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Column(nullable = false)
   private Long memberId;
 
   @ManyToOne
