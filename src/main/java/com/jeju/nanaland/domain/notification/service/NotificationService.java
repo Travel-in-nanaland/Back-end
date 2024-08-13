a5ef7cb17f80490f79b71eb962e065486a6dda20
seokhee <skdlzl753@naver.com>
2024-08-13 13:52:02 +0900

    [#319] feat: 알림 클릭 이벤트 정보 추가

--- src/main/java/com/jeju/nanaland/domain/notification/service/NotificationService.java
+++ src/main/java/com/jeju/nanaland/domain/notification/service/NotificationService.java
@@ -224,7 +224,7 @@ public class NotificationService {
                 .map(FcmToken::getToken)
                 .toList())
         // 알림 내용 정보 - data
-        .putData("clickAction", notificationDto.getCategory().getClickEvent().name())
+        .putData("clickEvent", notificationDto.getCategory().getClickEvent().name())
         .putData("category", notificationDto.getCategory().name())
         .putData("contentId", notificationDto.getContentId().toString())
         // 공통 알림 정보 - notification
@@ -258,7 +258,7 @@ public class NotificationService {
         // 수신 측 토큰 정보 - token
         .setToken(notificationWithTargetDto.getTargetToken())
         // 알림 내용 정보 - data
-        .putData("clickAction", notificationDto.getCategory().getClickEvent().name())
+        .putData("clickEvent", notificationDto.getCategory().getClickEvent().name())
         .putData("category", notificationDto.getCategory().name())
         .putData("contentId", notificationDto.toString())
         // 공통 알림 정보 - notification
