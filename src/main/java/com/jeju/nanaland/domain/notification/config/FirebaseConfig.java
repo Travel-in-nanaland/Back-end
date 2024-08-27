23e0428a9a5720b7ca985ab176da35f5311b902c
hseoky desktop <skdlzl753@naver.com>
2024-08-27 16:46:05 +0900

    [#319] feat: firebase config 수정

--- src/main/java/com/jeju/nanaland/domain/notification/config/FirebaseConfig.java
+++ src/main/java/com/jeju/nanaland/domain/notification/config/FirebaseConfig.java
@@ -4,25 +4,33 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
-import java.io.FileInputStream;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 @Slf4j
 public class FirebaseConfig {
 
+  @Value("${firebase.credential.resource}")
+  private String resourcePath;
+
   @PostConstruct
   public void init() {
     try {
-      FileInputStream serviceAccount =
-          new FileInputStream("src/main/resources/firebase/nanaland-firebase-key.json");
       FirebaseOptions options = FirebaseOptions.builder()
-          .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+          .setCredentials(GoogleCredentials.fromStream(
+              new ClassPathResource(resourcePath)
+                  .getInputStream()
+          ))
           .build();
-      FirebaseApp.initializeApp(options);
+      if (FirebaseApp.getApps().isEmpty()) {
+        FirebaseApp.initializeApp(options);
+        log.info("FirebaseApp initialized");
+      }
     } catch (Exception e) {
-      log.info("Firebase configuration error:", e.getMessage());
+      log.info("Firebase configuration error:", e);
     }
   }
 }
