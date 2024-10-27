32ed7755e141804970d3b49a993de053b01afac2
jyajoo <jyajoo1020@gmail.com>
2024-10-28 02:53:49 +0900

    [#466] feat: cors 설정 추가

--- src/main/java/com/jeju/nanaland/global/config/SecurityConfig.java
+++ src/main/java/com/jeju/nanaland/global/config/SecurityConfig.java
@@ -19,6 +19,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -37,7 +40,7 @@ public class SecurityConfig {
         .httpBasic(AbstractHttpConfigurer::disable)
         .formLogin(AbstractHttpConfigurer::disable)
         .logout(AbstractHttpConfigurer::disable)
-        .cors(AbstractHttpConfigurer::disable)
+        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
         .headers(c -> c.frameOptions(
             FrameOptionsConfig::disable).disable())
         .sessionManagement(c ->
@@ -78,5 +81,23 @@ public class SecurityConfig {
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+
+    configuration.addAllowedOrigin("http://13.125.110.80:8083");
+    configuration.addAllowedOrigin("https://nanaland.site");
+
+    configuration.setAllowCredentials(true);
+    configuration.addAllowedHeader("*");
+    configuration.addAllowedMethod("*");
+    configuration.setMaxAge(3600L);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+
+    return source;
   }
 }
