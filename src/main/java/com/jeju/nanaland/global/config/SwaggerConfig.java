a35aae3f3683c597498e11e9a494e2130facb487
Te___ho <54144849+Te-H0@users.noreply.github.com>
2024-10-28 14:53:21 +0900

    [#471] refactor: swagger https 적용

--- src/main/java/com/jeju/nanaland/global/config/SwaggerConfig.java
+++ src/main/java/com/jeju/nanaland/global/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.jeju.nanaland.global.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -10,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(servers = {@Server(url = "https://nanaland.site")})
 public class SwaggerConfig {
 
   @Value("${springdoc.version}")
