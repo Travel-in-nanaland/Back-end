30dbe942f7e07a1d90a6bda0fb0ce262f0314b49
jyajoo <jyajoo1020@gmail.com>
2024-10-28 18:29:36 +0900

    [#472] fix: 파일 변환 시 버퍼 설정 추가

--- src/main/java/com/jeju/nanaland/global/util/CustomMultipartFile.java
+++ src/main/java/com/jeju/nanaland/global/util/CustomMultipartFile.java
@@ -1,9 +1,13 @@
 package com.jeju.nanaland.global.util;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import lombok.AllArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,6 +19,7 @@ public class CustomMultipartFile implements MultipartFile {
   private final String name;
   private final String originalFilename;
   private final String contentType;
+  private static final int BUFFER_SIZE = 8192;
 
   @Override
   public String getName() {
@@ -48,11 +53,18 @@ public class CustomMultipartFile implements MultipartFile {
 
   @Override
   public InputStream getInputStream() throws IOException {
-    return new FileInputStream(file);
+    return new BufferedInputStream(new FileInputStream(file), BUFFER_SIZE);
   }
 
   @Override
   public void transferTo(File dest) throws IOException, IllegalStateException {
-    Files.copy(file.toPath(), dest.toPath());
+    try (InputStream in = getInputStream();
+        OutputStream out = new BufferedOutputStream(new FileOutputStream(dest), BUFFER_SIZE)) {
+      byte[] buffer = new byte[BUFFER_SIZE];
+      int bytesRead;
+      while ((bytesRead = in.read(buffer)) != -1) {
+        out.write(buffer, 0, bytesRead);
+      }
+    }
   }
 }
