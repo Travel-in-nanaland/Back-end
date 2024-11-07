7d72347f29004ccaf50a3b47d6b824e286989f5b
jyajoo <jyajoo1020@gmail.com>
2024-11-07 17:21:42 +0900

    [#495] refactor: S3Config 파일 이동

--- src/main/java/com/jeju/nanaland/global/config/S3Config.java
+++ src/main/java/com/jeju/nanaland/global/config/S3Config.java
@@ -1,15 +1,13 @@
-package com.jeju.nanaland.global.image_upload;
+package com.jeju.nanaland.global.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Slf4j
 @Configuration
 public class S3Config {
 
